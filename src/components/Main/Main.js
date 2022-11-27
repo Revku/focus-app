a702238ea16e2cbf62dcc86c4dc300690506c6ef
Revku <office@revku.dev>
2022-11-27 20:06:12 +0100

    Timer logic implementation

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,32 +3,79 @@ import button from 'components/Button/Button.module.scss';
 import styles from './Main.module.scss';
 
 const Main = () => {
+  const modes = {
+    pomodoro: 25,
+    shortbreak: 5,
+    longbreak: 20,
+  };
+
   const [active, setActive] = React.useState('pomodoro');
-  const [timer, setTimer] = React.useState('0:00');
+  const [timer, setTimer] = React.useState('00:00');
+  const [isActive, setIsActive] = React.useState(false);
+  const [secondsLeft, setSecondsLeft] = React.useState(modes.pomodoro * 60);
+  const [buttonText, setButtonText] = React.useState('start');
+
+  const formatTimeLeft = (seconds) => {
+    return `${Math.floor(seconds / 60)}:${
+      seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`
+    }`;
+  };
 
   React.useEffect(() => {
     switch (active) {
       case 'pomodoro':
-        setTimer('25:00');
+        setSecondsLeft(modes.pomodoro * 60);
         break;
       case 'shortbreak':
-        setTimer('05:00');
+        setSecondsLeft(modes.shortbreak * 60);
         break;
       case 'longbreak':
-        setTimer('20:00');
+        setSecondsLeft(modes.longbreak * 60);
         break;
       default:
-        setTimer('25:00');
+        setSecondsLeft(modes.pomodoro * 60);
         break;
     }
+
+    setIsActive(false);
   }, [active]);
+
+  React.useEffect(() => {
+    if (isActive) {
+      const interval = setInterval(
+        () => setSecondsLeft((secondsLeft) => secondsLeft - 1),
+        1000
+      );
+      setButtonText('stop');
+
+      if (secondsLeft === 0) {
+        clearInterval(interval);
+        setIsActive(false);
+        setButtonText('start');
+      }
+
+      return () => clearInterval(interval);
+    }
+
+    if (isActive === false) {
+      setButtonText('start');
+    }
+  }, [isActive]);
+
+  React.useEffect(() => {
+    setTimer(formatTimeLeft(secondsLeft));
+  }, [secondsLeft]);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
         <p className={styles.timer}>{timer}</p>
-        <button type="button" className={button.normal}>
-          rozpocznij
+        <button
+          type="button"
+          className={button.normal}
+          onClick={() => setIsActive(!isActive)}
+        >
+          {buttonText}
         </button>
       </div>
 
