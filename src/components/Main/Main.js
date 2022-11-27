75b7862558deb101e63db0d6889c63d2a58cbc2b
Revku <office@revku.dev>
2022-11-27 18:58:48 +0100

    Add menu buttons interaction

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,23 +3,61 @@ import button from 'components/Button/Button.module.scss';
 import styles from './Main.module.scss';
 
 const Main = () => {
+  const [active, setActive] = React.useState('pomodoro');
+  const [timer, setTimer] = React.useState('0:00');
+
+  React.useEffect(() => {
+    switch (active) {
+      case 'pomodoro':
+        setTimer('25:00');
+        break;
+      case 'shortbreak':
+        setTimer('05:00');
+        break;
+      case 'longbreak':
+        setTimer('20:00');
+        break;
+      default:
+        setTimer('25:00');
+        break;
+    }
+  }, [active]);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        <p className={styles.timer}>25:00</p>
+        <p className={styles.timer}>{timer}</p>
         <button type="button" className={button.normal}>
           rozpocznij
         </button>
       </div>
 
       <div className={styles.buttons}>
-        <button type="button" className={`${button.normal} ${styles.button}`}>
+        <button
+          type="button"
+          className={`${active === 'pomodoro' ? button.normal : button.ghost} ${
+            styles.button
+          }`}
+          onClick={() => setActive('pomodoro')}
+        >
           pomodoro
         </button>
-        <button type="button" className={`${button.ghost} ${styles.button}`}>
+        <button
+          type="button"
+          className={`${
+            active === 'shortbreak' ? button.normal : button.ghost
+          } ${styles.button}`}
+          onClick={() => setActive('shortbreak')}
+        >
           short break
         </button>
-        <button type="button" className={`${button.ghost} ${styles.button}`}>
+        <button
+          type="button"
+          className={`${
+            active === 'longbreak' ? button.normal : button.ghost
+          } ${styles.button}`}
+          onClick={() => setActive('longbreak')}
+        >
           long break
         </button>
       </div>
