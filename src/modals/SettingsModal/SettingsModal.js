94dff6477f35025a53fdc00f59dbc4d7078f3e48
Revku <office@revku.dev>
2022-12-03 11:03:40 +0100

    Add settings save function

--- src/modals/SettingsModal/SettingsModal.js
+++ src/modals/SettingsModal/SettingsModal.js
@@ -2,10 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { AnimatePresence } from 'framer-motion';
 import Modal from 'components/Modal/Modal';
+import saveSettings from 'hooks/saveSettings';
 import button from 'components/Button/Button.module.scss';
+import useSettings from 'hooks/useSettings';
 import styles from './SettingsModal.module.scss';
 
 const SettingsModal = ({ status, setStatus }) => {
+  const settings = useSettings();
+
+  const [pomodoro, setPomodoro] = React.useState(settings.timers.pomodoro);
+  const [shortbreak, setShortbreak] = React.useState(
+    settings.timers.shortbreak
+  );
+  const [longbreak, setLongbreak] = React.useState(settings.timers.longbreak);
+
+  const handleSave = () => {
+    settings.timers = {
+      pomodoro: parseInt(pomodoro),
+      shortbreak: parseInt(shortbreak),
+      longbreak: parseInt(longbreak),
+    };
+
+    saveSettings(settings);
+    setStatus(false);
+    // Temponary resolution
+    location.reload();
+  };
+
   return (
     <AnimatePresence
       initial={false}
@@ -24,6 +47,8 @@ const SettingsModal = ({ status, setStatus }) => {
                 type="number"
                 id="pomodoro"
                 placeholder="25"
+                value={pomodoro}
+                onChange={(event) => setPomodoro(event.target.value)}
               />
               <p className={styles.suffix}>minutes</p>
             </div>
@@ -37,6 +62,8 @@ const SettingsModal = ({ status, setStatus }) => {
                 type="number"
                 id="shortbreak"
                 placeholder="5"
+                value={shortbreak}
+                onChange={(event) => setShortbreak(event.target.value)}
               />
               <p className={styles.suffix}>minutes</p>
             </div>
@@ -50,12 +77,14 @@ const SettingsModal = ({ status, setStatus }) => {
                 type="number"
                 id="longbreak"
                 placeholder="15"
+                value={longbreak}
+                onChange={(event) => setLongbreak(event.target.value)}
               />
               <p className={styles.suffix}>minutes</p>
             </div>
           </div>
           <div className={styles.footer}>
-            <button type="button" className={button.ghost}>
+            <button onClick={handleSave} type="button" className={button.ghost}>
               Save settings
             </button>
           </div>
