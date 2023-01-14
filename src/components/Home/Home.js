04b1d0cb869e7c32c49da41cb43746fa007dc8c8
Revku <revku@revku.dev>
2023-01-14 15:39:41 +0100

    Add app loader

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,22 +4,48 @@ import Main from 'components/Main/Main';
 import Footer from 'components/Footer/Footer';
 import { store } from 'store/index';
 import { Provider } from 'react-redux';
+import { ThreeDots } from 'react-loader-spinner';
+import brain from 'assets/brain.svg';
 import styles from './Home.module.scss';
 
 const Home = () => {
+  const [loaded, setLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    window.addEventListener('load', () => {
+      setLoaded(true);
+    });
+  }, []);
+
   return (
     <Provider store={store}>
-      <div className={styles.wrapper}>
-        <div>
-          <Navigation />
+      {loaded ? (
+        <div className={styles.wrapper}>
+          <div>
+            <Navigation />
+          </div>
+          <div className={styles.main}>
+            <Main />
+          </div>
+          <div>
+            <Footer />
+          </div>
         </div>
-        <div className={styles.main}>
-          <Main />
+      ) : (
+        <div className={styles.loader}>
+          <img src={brain} alt="Focus Logo" />
+          <ThreeDots
+            height="80"
+            width="80"
+            radius="9"
+            color="#999"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+          />
         </div>
-        <div>
-          <Footer />
-        </div>
-      </div>
+      )}
     </Provider>
   );
 };
